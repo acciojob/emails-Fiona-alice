db3de4918af90cab123821b159cc7e51b53a0541
Fiona-alice <alicce.fiona@gmail.com>
2022-12-24 21:53:24 +0300

    email

--- src/main/java/com/driver/Gmail.java
+++ src/main/java/com/driver/Gmail.java
@@ -6,9 +6,15 @@ import java.util.Date;
 public class Gmail extends Email {
 
     int inboxCapacity; //maximum number of mails inbox can store
+    String Inbox;
+    String Trash;
     //Inbox: Stores mails. Each mail has date (Date), sender (String), message (String). It is guaranteed that message is distinct for all mails.
     //Trash: Stores mails. Each mail has date (Date), sender (String), message (String)
     public Gmail(String emailId, int inboxCapacity) {
+       super(emailId);
+       this.inboxCapacity = inboxCapacity;
+       this.Inbox = new ArrayList<>();
+       this.Trash = new ArrayList<>();
 
     }
 
